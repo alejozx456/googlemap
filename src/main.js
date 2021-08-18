1345a1d62730bc9bd49024173dae1b91d4698753
unknown <alejozx456@gmail.com>
2021-08-18 18:44:02 -0500

    first commit

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import App from "./App.vue";
 import * as VueGoogleMaps from "vue2-google-maps";
+import router from './router'
 
 Vue.config.productionTip = false;
 
@@ -11,5 +12,6 @@ Vue.use(VueGoogleMaps, {
 });
 
 new Vue({
-  render: (h) => h(App),
+  router,
+  render: (h) => h(App)
 }).$mount("#app");
---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+   <h1>Hogar</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+
+export default {
+  name: 'Home',
+ 
+}
+</script>
